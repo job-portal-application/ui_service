c8593d31ecb302acc18a42a73b1388fd42fbf002
SUVAMROY\roysu <roysuvam460@gmail.com>
2026-08-14 22:08:57 +0530

    nginx fixes more and

--- src/config/__mocks__/env.ts
+++ src/config/__mocks__/env.ts
@@ -1,5 +1,5 @@
 export const env = {
-  aiServiceBaseUrl: 'http://localhost:3001',
-  authServiceBaseUrl: 'http://localhost:3002',
-  userServiceBaseUrl: 'http://localhost:3003',
+  aiServiceBaseUrl: '',
+  authServiceBaseUrl: '',
+  userServiceBaseUrl: '',
 } as const;
