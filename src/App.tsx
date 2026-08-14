adaf6263a7e8ee3d38f2d1907b7075806453cb95
SUVAMROY\roysu <roysuvam460@gmail.com>
2026-08-14 20:34:06 +0530

    Fixed minimal issues

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Navbar from './components/Navbar';
 import AppRoutes from './routes/AppRouters';
 import { ThemeProvider } from './components/ThemeProvider';
 import { Toaster } from 'react-hot-toast';
-import { useAppDispatch, useAppSelector } from './redux/hooks';
+import { useAppDispatch } from './redux/hooks';
 import { useEffect, useState } from 'react';
 import Loader from './components/Loader';
 import { useNavigate } from 'react-router-dom';
@@ -14,7 +14,6 @@ import PageWrapper from './components/PageWrapper';
 function App() {
   const dispatch = useAppDispatch();
   const location = useNavigate();
-  const loading = useAppSelector((state: any) => state.loader.loading);
   const [initialLoading, setInitialLoading] = useState<boolean>(false);
 
   useEffect(() => {
